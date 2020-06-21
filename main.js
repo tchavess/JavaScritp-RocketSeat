e175fee2bae8529a2902f7b11c03cdd0df7f170f
Thiago Chaves <thiago.chaves04@gmail.com>
2020-06-21 00:46:58 -0300

    Requisição AJAX com Axios

--- main.js
+++ main.js
@@ -1,23 +1,4 @@
-var minhaPromise = function(){
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET','https://api.github.com/users/tchavess');
-        xhr.send(null);
-
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4){
-                if(xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText));
-                }else{
-                    reject('Erro na requisicao');
-                }
-            }
-        }
-    });
-}
-
-
-minhaPromise()
+axios.get('https://pokeapi.co/api/v2/pokemon/')
 .then(function(response){
     console.log(response);
 })
