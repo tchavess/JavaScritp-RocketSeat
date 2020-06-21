dd0008315af6bea015dce7a20b7ab4de8e1850de
Thiago Chaves <thiago.chaves04@gmail.com>
2020-06-21 12:59:57 -0300

    Desafio modulo 4

--- main.js
+++ main.js
@@ -1,7 +1,44 @@
-axios.get('https://pokeapi.co/api/v2/pokemon/')
-.then(function(response){
-    console.log(response);
-})
-.catch(function(error){
-console.warn(error)
-})
+//modulo 4 
+
+//EXERCICIO 1
+var idade = parseInt(prompt('Digite sua idade: '),10);
+
+function checaIdade(idade){
+    return new Promise((resolve, reject) =>
+      
+        setTimeout(() => idade >= 18 ? resolve() : reject(), 2000)
+    
+    )
+}
+
+checaIdade(idade)
+.then(() => console.log('Maior de 18 anos'))
+.catch(() => console.log("Menor de 18 anos"))
+
+//FIM EXERCICIO 1
+
+//exercicio 2
+var buttonUserElement = document.querySelector('#btnUser');
+buttonUserElement.onclick = function buscaUser(){
+    var user = document.querySelector('#inputUser').value;
+    if (!user) return;
+    axios.get('https://api.github.com/users/'+ user + '/repos')
+    .then(function(response){
+        renderRepositories(response.data)
+    }).catch(function(error){
+        console.warn(error)
+    })
+}
+//fim exercicio 2
+
+//Exercicio 3
+var listElement = document.querySelector('#ulUser');
+function renderRepositories(repositories){
+    
+    for(repo of repositories){
+        var liElement = document.createElement('li');
+        var textElement = document.createTextNode(repo.name);
+        liElement.appendChild(textElement);
+        listElement.appendChild(liElement);
+    }
+}
